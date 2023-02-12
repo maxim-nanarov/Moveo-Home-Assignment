bf97f2b13032087b8f06021cf98bb17f03173c30
student <you@example.com>
2023-02-12 14:09:52 +0200

    founded out how to create code blocks

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,30 @@
-import React from "react";
-//The lobby
-export default function Lobby() {
-  return <div>lobby</div>;
-}
+import React, { useState } from "react";
+import hljs from "highlight.js";
+import "highlight.js/styles/default.css";
+
+const CodeBlock = ({ title, code }) => {
+  const [currentCode, setCurrentCode] = useState(code);
+  const codeRef = React.createRef();
+
+  const handleCodeUpdate = (event) => {
+    setCurrentCode(event.target.value);
+  };
+
+  React.useEffect(() => {
+    hljs.highlightBlock(codeRef.current);
+  }, [currentCode]);
+
+  return (
+    <div className="code-block">
+      <h2>{title}</h2>
+      <textarea value={currentCode} onChange={handleCodeUpdate} />
+      <pre>
+        <code ref={codeRef} className="javascript">
+          {currentCode}
+        </code>
+      </pre>
+    </div>
+  );
+};
+
+export default CodeBlock;
