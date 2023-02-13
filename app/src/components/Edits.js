154713a2f2e28035fd1c30042941a2c4b48f713d
student <you@example.com>
2023-02-13 19:55:42 +0200

    mongodb works added some styling

--- app/src/components/Edits.js
+++ app/src/components/Edits.js
@@ -2,7 +2,8 @@ import React from "react";
 import { useState, useEffect } from "react";
 import CodeEditor from "react-simple-code-editor";
 import { highlight, languages } from "prismjs/components/prism-core";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import axios from "axios";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
 import "prismjs/themes/prism.css";
@@ -14,15 +15,29 @@ const Edits = () => {
   const [code, setCode] = useState(`console.log('Hello World');`);
   let { state } = useLocation();
   useEffect(() => {
+    console.log(state);
     setCode(state.Code);
   }, []);
 
   const handleValueChange = (value) => {
     setCode(value);
   };
+  ///?id=${state._id}
+  function Update() {
+    axios
+      .put(`http://localhost:2999/Code-Blocks-Update/${state._id}`, {
+        code,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   return (
-    <div>
+    <div className="Edit">
       <h1>Edit</h1>
       <CodeEditor
         value={code}
@@ -34,6 +49,10 @@ const Edits = () => {
           fontSize: 12,
         }}
       />
+      <div>
+        <button onClick={Update}>Save</button>
+        <Link to="/mainmenu">Back</Link>
+      </div>
     </div>
   );
 };
--- app/src/components/Lobby.js
+++ app/src/components/Lobby.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 //the lobby page where the users first see when entering the application
 export default function Lobby() {
   return (
-    <div>
+    <div className="Lobby">
       <h1>Lobby</h1>
       <Link to="mainmenu">Enter</Link>
     </div>
